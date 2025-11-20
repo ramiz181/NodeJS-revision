6d7715e28b3e7d68aa514555a0b2427ba333c5e6
ramiz181 <ramizmalik78685@gmail.com>
2025-11-21 02:07:56 +0500

    minor changes in server file

--- 2- creating node server/index.js
+++ 2- creating node server/index.js
@@ -2,16 +2,19 @@ import http from 'http'
 import fs from 'fs'
 import os from 'os'
 
-
 console.log(os.cpus().length);
 
-const server = http.createServer((req, res) => {
+const PORT = 3000
 
-    const logs = `${Date.now()} ${req.url}  New Client Request...\n`
+const server = http.createServer((req, res) => {
+    
+    // req parameter may carry all the information of user.... ip address, kis route/url sy req i etc
+
+    const logs = `${Date.now()} localhost${PORT}${req.url}  New Client Request...\n`
 
     fs.appendFile('client.txt', logs, () => {
         res.end("Homepage")
     })
 })
 
-server.listen(3000, () => console.log("Server started...!"));
+server.listen(PORT, () => console.log("Server started...!"));
