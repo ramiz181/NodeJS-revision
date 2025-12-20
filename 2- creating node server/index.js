7668a0c92eb5f4a11c77fe8d0b143894b3c06655
ramiz181 <ramizmalik78685@gmail.com>
2025-12-21 00:29:19 +0500

    fs module

--- 2- creating node server/index.js
+++ 2- creating node server/index.js
@@ -4,15 +4,20 @@ import os from 'os'
 
 console.log(os.cpus().length);
 
+const PORT = 3000
+
 const server = http.createServer((req, res) => {
 
     // req parameter may carry all the information of user.... ip address, kis route/url sy req i etc
-    const logs = `${Date.now()} localhost${PORT}${req.url}  New Client Request...\n`
+    const logs = `${Date.now()} localhost:${PORT}${req.url}  New Client Request...\n`
 
     fs.appendFile('client.txt', logs, (err) => {
         if (err) throw err
-
         res.end("Homepage")
     })
+    fs.stat('client.txt', (err, stats) => {
+        if (err) throw err
+        console.log(stats.isFile());
+    })
 })
-server.listen(3000, () => console.log("Server started...!"));
+server.listen(PORT, () => console.log("Server started...!"));
