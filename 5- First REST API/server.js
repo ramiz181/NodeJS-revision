6dab6e85859a33602223ab15b7dfda1d957529bd
ramiz181 <ramizmalik78685@gmail.com>
2026-01-01 19:06:49 +0500

    created first REST API

---
+++ 5- First REST API/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import users from './USERS.json' with { type: 'json' }
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    const html = `
+    <ul> 
+        ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html)
+})
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    let user
+    if (req.params.id === "female") {
+        user = users.filter(user => user.gender === "Female")
+    }
+    else if (req.params.id === "male") {
+        user = users.filter(user => user.gender === "Male")
+    }
+    else {
+        user = users.find(user => user.id === id)
+    }
+    res.json(user)
+})
+
+app.listen(3000, () => console.log('started...'))
