eea3fc7cbda2888782d56367a749e403e0c768ee
ramiz181 <ramizmalik78685@gmail.com>
2026-01-03 01:08:41 +0500

    server.js

--- 5- REST API POST req/server.js
+++ 5- REST API POST req/server.js
@@ -14,6 +14,7 @@ app.get('/api/users/:id', (req, res) => {
 // const id = req.params.id
 app.post('/api/users', (req, res) => {
 
+    // spread the body....add id key...
     users.push({ ...req.body, id: users.length + 1 })
 
     fs.writeFile("USERS.json", JSON.stringify(users), (error) => {
