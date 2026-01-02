a542014e7ac3ce899773c517f5a6bd5e96082b78
ramiz181 <ramizmalik78685@gmail.com>
2026-01-03 01:07:17 +0500

    created notes.txt, some fixes in server file

--- 5- REST API POST req/server.js
+++ 5- REST API POST req/server.js
@@ -1,24 +1,25 @@
-import express, { urlencoded } from 'express'
+import express from 'express'
 import fs from 'fs'
 import users from './USERS.json' with {type: 'json'}
 
 const app = express()
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("sending request to server...")
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find(user => user.id === id)
+    res.json(user)
 })
 
 // const id = req.params.id
 app.post('/api/users', (req, res) => {
-    const body = req.body;
-    // console.log(body);
-    users.push(body)
+
+    users.push({ ...req.body, id: users.length + 1 })
 
     fs.writeFile("USERS.json", JSON.stringify(users), (error) => {
         if (error) throw error
     })
-    return res.json({ message: "data sent to server..." })
+    return res.json({ message: "User saved successfully..." })
 })
 
-app.listen(3000, () => console.log("..."));
+app.listen(3000, () => console.log("server started"));
