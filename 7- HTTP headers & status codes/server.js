0299efc2890ff49dbb23c31ee522b850596827f5
ramiz181 <ramizmalik78685@gmail.com>
2026-01-07 20:02:55 +0500

    HTTP request/response headers

--- 7- HTTP headers & status codes/server.js
+++ 7- HTTP headers & status codes/server.js
@@ -8,16 +8,18 @@ app.post('/api/header', (req, res) => {
 
     // use X- before custom header name to identify it as a custom header
     // built-in headers don't have X- before header name
+    // response headers ==>> server ny jo headers bhejy
     res.setHeader('X-token', '99328HRD3AS')
-    res.send("sending headers")
+    res.send("server-side headers")
 })
 
 app.get('/api/header/', (req, res) => {
     // console.log(req.headers)
     // console.log(req.headers.token)
+    // req.headers ==>> client ny jo headers bhejy 
     console.log("line 18", req.headers);
 
-    res.send("hell")
+    res.send("client-side headers")
 })
 
 app.listen(PORT, () => console.log("Server started"));
