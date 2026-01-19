a1430f55eaa71b114940bea016e196e232ac9e49
ramiz181 <ramizmalik78685@gmail.com>
2026-01-19 08:17:14 +0500

    req.headers.cookie

--- 11- JWT authentication/src/middlewares/auth.middleware.js
+++ 11- JWT authentication/src/middlewares/auth.middleware.js
@@ -5,6 +5,10 @@ import { verifyToken } from "../services/auth.service.js"
 export const restrictToLoggedInUserOnly = (req, res, next) => {
 
     const userUuid = req.cookies?.uuid
+    // const userUuid = req.headers.cookie    // ===> valie
+
+    console.log("Line 10  middleware", userUuid);
+
     if (!userUuid) return res.redirect('/login?error=User%not%logged-in')
 
     const user = verifyToken(userUuid)
