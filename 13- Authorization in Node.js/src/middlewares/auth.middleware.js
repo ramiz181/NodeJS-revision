4c73c381733500e13ed6d18b5c620130f64efca1
ramiz181 <ramizmalik78685@gmail.com>
2026-01-21 20:01:07 +0500

    admin only route created

--- 13- Authorization in Node.js/src/middlewares/auth.middleware.js
+++ 13- Authorization in Node.js/src/middlewares/auth.middleware.js
@@ -5,16 +5,15 @@ export const authentication = (req, res, next) => {
     try {
 
         const token = req.cookies?.token
-        
+
         if (!token) return res.redirect('/login?error=Invalid%request')
-        
+
         const user = verifyToken(token)
         if (!user) return res.redirect('/login?error=UnAuthentic%User')
         req.user = user
         next()
 
     } catch (error) {
-
         console.log("middleware error", error);
     }
 }
@@ -22,10 +21,7 @@ export const authentication = (req, res, next) => {
 export const restrictTo = (roles = []) => {
 
     return function (req, res, next) {
-        // if (!req.user) return res.redirect('/login?error=UnAuthentic%User')
-
-        console.log("line 21", req.user.role);
-
+        if (!req.user) return res.redirect('/login?error=UnAuthentic%User')
         if (!roles.includes(req.user.role)) return res.end("UnAuthorized User")
         next()
     }
