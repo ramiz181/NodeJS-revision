8f1eb32ec92febb3c3a308eef06b9e231ab57e7a
ramiz181 <ramizmalik78685@gmail.com>
2026-01-21 17:54:57 +0500

    implmented authorization middleware

---
+++ 13- Authorization in Node.js/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import path from 'path'
+import { dbAuthConnection, dbURLConnection } from './src/config/db.config.js';
+import { authentication, restrictTo } from './src/middlewares/auth.middleware.js';
+import router from './src/routes/user.routes.js';
+import staticRoutes from './src/routes/static.routes.js';
+import urlRoutes from './src/routes/url.routes.js';
+import { URL } from './src/models/url.model.js';
+
+const app = express()
+const PORT = 5001
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cookieParser())
+dotenv.config();
+
+dbAuthConnection
+dbURLConnection
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./src/views'))
+
+app.use('/user', router)
+app.use('/', staticRoutes)
+
+app.use(authentication)
+app.use('/url', restrictTo('user'), urlRoutes)
+app.use('/',  restrictTo('user'), async (req, res) => {
+
+    const entries = await URL.find({ createdBy: req.user._id })
+    res.render('home', {
+        entries
+    })
+})
+
+app.listen(PORT, () => console.log("Server started"));
---
+++ 13- Authorization in Node.js/src/controllers/url.controllers.js
@@ -0,0 +1,46 @@
+import { nanoid } from "nanoid"
+import { URL } from "../models/url.model.js"
+
+export const handleGenerateURL = async (req, res) => {
+
+    const { redirectURL } = req.body
+    if (!redirectURL) res.status(400).json({ error: 'URL is required' })
+
+    const shortID = nanoid(8)
+
+    await URL.create({
+        shortURL: shortID,
+        redirectURL,
+        createdBy: req.user._id
+    })
+    res.render('home', {
+        shortID
+    })
+    // res.status(200).json({ status: 'success', message: 'short URL created' })
+}
+
+export const handleRedirectURL = async (req, res) => {
+
+    const shortURL = req.params.shortID
+
+    const entry = await URL.findOneAndUpdate({ shortURL }, {
+        $push: {
+            visitorHistory: {
+                timestamp: new Date().toLocaleString()
+            }
+        }
+    })
+    res.redirect(entry.redirectURL)
+    // res.send("Hello")
+}
+
+export const handleGetAnalytics = async (req, res) => {
+
+    const shortURL = req.params.shortID
+
+    const result = await URL.findOne({ shortURL })
+    res.json({
+        totalClicks: result.visitorHistory.length,
+        analytics: result.visitorHistory
+    })
+}
