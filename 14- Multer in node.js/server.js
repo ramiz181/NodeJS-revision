db29ee2c41823dce303e526309d976d83c1aeaeb
ramiz181 <ramizmalik78685@gmail.com>
2026-01-22 14:31:29 +0500

    multer middleware

--- 14- Multer in node.js/server.js
+++ 14- Multer in node.js/server.js
@@ -1,17 +1,40 @@
 import express from 'express'
 import path from 'path'
-
+import multer from 'multer'
 const app = express()
 const PORT = 5001
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./src/views'))
 
 
-app.use(express.json())
+
+// const upload = multer({ dest: 'upload/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        return cb(null, uniqueSuffix + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 app.get('/', (req, res) => {
-    res.render("homepage")
+    res.render("home", {
+        username: "Ramiz"
+    })
+})
+
+app.post('/profile', upload.single('profileImage'), (req, res) => {
+    console.log(req.file)
+
+    res.redirect('/')
 })
 
 app.listen(PORT, () => console.log("Server started"));
